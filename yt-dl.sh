2e0fbf3040823251362e9024134828260c275b20
iMaNeX <imnx.au@outlook.com>
2017-04-18 00:21:24 +1000

    Update yt-dl.sh

--- yt-dl.sh
+++ yt-dl.sh
@@ -1,1 +1,9 @@
-
+#!/bin/bash
+if [[ ${1} == "--batch" ]]; then
+shift
+youtube-dl --yes-playlist --prefer-ffmpeg --ffmpeg-location /usr/bin/ffmpeg --audio-format mp3 --audio-quality 192k --recode-video mp4 \
+--format mp4 --rate-limit 1000k --retries 5 --force-ipv4 --socket-timeout 15 --no-check-certificate --prefer-insecure --batch-file $@
+else
+youtube-dl --yes-playlist --prefer-ffmpeg --ffmpeg-location /usr/bin/ffmpeg --audio-format mp3 --audio-quality 192k --recode-video mp4 \
+--format mp4 --rate-limit 1000k --retries 5 --force-ipv4 --socket-timeout 15 --no-check-certificate --prefer-insecure --batch-file $@
+fi
